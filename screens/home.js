7646aea6c70c636200c6b5d67c3f8f5137f254bd
JAParsons <johnparsons932@gmail.com>
2020-09-12 10:30:17 +0100

    Testing out navigaton with buttons

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default Home = () => {
+export default Home = ({ navigation }) => {
+  const pressHandler = () => {
+    navigation.push('ReviewDetails');
+  };
+
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Home Screen</Text>
+      <Button title="go" onPress={pressHandler} />
     </View>
   );
 };
--- screens/review-details.js
+++ screens/review-details.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default ReviewDetails = () => {
+export default ReviewDetails = ({ navigation }) => {
+  const pressHandler = () => {
+    navigation.pop();
+  };
+
   return (
     <View style={globalStyles.container}>
-      <Text>ReviewDetails Screen</Text>
+      <Text style={globalStyles.titleText}>ReviewDetails Screen</Text>
+      <Button title="go back" onPress={pressHandler} />
     </View>
   );
 };
