9fe0670ebb2a15bad60458f22c51850ee7c90e70
JAParsons <johnparsons932@gmail.com>
2020-09-12 09:59:36 +0100

    Global styling

--- screens/review-details.js
+++ screens/review-details.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
+import { globalStyles } from '../styles/global';
 
 export default ReviewDetails = () => {
   return (
-    <View style={styles.container}>
+    <View style={globalStyles.container}>
       <Text>ReviewDetails Screen</Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({ container: { padding: 24 } });
