18751bc15dad55c332c7fef439349185f952d3ca
JAParsons <johnparsons932@gmail.com>
2020-09-12 08:57:14 +0100

    :tada: Added placeholder screens

---
+++ screens/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default About = () => {
+  return (
+    <View style={styles.container}>
+      <Text>About Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({ container: { padding: 24 } });
---
+++ screens/review-details.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default ReviewDetails = () => {
+  return (
+    <View style={styles.container}>
+      <Text>ReviewDetails Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({ container: { padding: 24 } });
