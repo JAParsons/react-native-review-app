97d9586adaafdef890c2519e8f2b8cd985d17ff3
JAParsons <johnparsons932@gmail.com>
2020-09-12 09:47:09 +0100

    Font loading

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
-import React from "react";
-import Home from "./screens/home";
+import React, { useState } from 'react';
+import { useFonts } from 'expo-font';
+import { AppLoading } from 'expo';
+import Home from './screens/home';
 
-export default function App() {
-  return <Home />;
-}
+export default App = () => {
+  const [fontsLoaded] = useFonts({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+  });
+
+  return !fontsLoaded ? <AppLoading /> : <Home />;
+};
